799385a03fc98a8f2ba11ed99748ca309a0ee17d
last order <2495713984@qq.com>
2019-12-13 20:16:54 +0800

    优化打包大小

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,22 @@ const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const path = require('path')
 
 module.exports = {
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: false
+    }
+  },
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       return {
         plugins: [
-          // new PrerenderSPAPlugin({
-          //   staticDir: path.join(__dirname, 'dist'),
-          //   routes: ['/']
-          // }),
+          new PrerenderSPAPlugin({
+            staticDir: path.join(__dirname, 'dist'),
+            routes: ['/']
+          }),
           new TerserPlugin({
+            parallel: true,
             terserOptions: {
               cache: true,
               compress: {
